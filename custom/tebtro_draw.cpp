0b7f09ec237782fd3c332d2ec3b1ae84da24b94d
Robert Ortner <robert.ortner.aut@gmail.com>
2020-08-12 21:00:46 +0200

    Error annotation use get_string_advance

--- custom/tebtro_draw.cpp
+++ custom/tebtro_draw.cpp
@@ -658,8 +658,6 @@ tebtro_draw_brace_highlight(Application_Links *app, Buffer_ID buffer_id, Text_La
 // @note: https://github.com/ryanfleury/4coder_fleury
 //        4coder_fleury.cpp
 //
-// @todo Maybe we don't want the text at the beginning: "error Cxxxx: "
-//
 inline void
 tebtro_draw_error_annotations(Application_Links *app, Buffer_ID buffer_id, Text_Layout_ID text_layout_id, Face_ID face_id, Buffer_ID jump_buffer_id) {
     Heap *heap = &global_heap;
@@ -669,81 +667,81 @@ tebtro_draw_error_annotations(Application_Links *app, Buffer_ID buffer_id, Text_
     // Face_ID face = global_small_code_face;
     Face_Metrics metrics = get_face_metrics(app, face_id);
     
-    if(jump_buffer_id != 0 && jump_state.view != 0)
+    if (jump_buffer_id == 0 || jump_state.view == 0) {
+        return;
+    }
+    
+    Managed_Scope buffer_scopes[2] = {
+        buffer_get_managed_scope(app, jump_buffer_id),
+        buffer_get_managed_scope(app, buffer_id),
+    };
+    
+    Managed_Scope comp_scope = get_managed_scope_with_multiple_dependencies(app, buffer_scopes, ArrayCount(buffer_scopes));
+    Managed_Object *buffer_markers_object = scope_attachment(app, comp_scope, sticky_jump_marker_handle, Managed_Object);
+    
+    // NOTE(rjf): Get buffer markers (locations where jumps point at).
+    i32 buffer_marker_count = 0;
+    Marker *buffer_markers = 0;
     {
-        Managed_Scope buffer_scopes[2] =
-        {
-            buffer_get_managed_scope(app, jump_buffer_id),
-            buffer_get_managed_scope(app, buffer_id),
-        };
-        
-        Managed_Scope comp_scope = get_managed_scope_with_multiple_dependencies(app, buffer_scopes, ArrayCount(buffer_scopes));
-        Managed_Object *buffer_markers_object = scope_attachment(app, comp_scope, sticky_jump_marker_handle, Managed_Object);
-        
-        // NOTE(rjf): Get buffer markers (locations where jumps point at).
-        i32 buffer_marker_count = 0;
-        Marker *buffer_markers = 0;
-        {
-            buffer_marker_count = managed_object_get_item_count(app, *buffer_markers_object);
-            buffer_markers = push_array(scratch, Marker, buffer_marker_count);
-            managed_object_load_data(app, *buffer_markers_object, 0, buffer_marker_count, buffer_markers);
+        buffer_marker_count = managed_object_get_item_count(app, *buffer_markers_object);
+        buffer_markers = push_array(scratch, Marker, buffer_marker_count);
+        managed_object_load_data(app, *buffer_markers_object, 0, buffer_marker_count, buffer_markers);
+    }
+    
+    i64 last_line = -1;
+    i64 code_line_number = -1;
+    
+    for (i32 i = 0; i < buffer_marker_count; i += 1, last_line = code_line_number) {
+        code_line_number = get_line_number_from_pos(app, buffer_id, buffer_markers[i].pos);
+        if (code_line_number == last_line) {
+            continue;
         }
         
-        i64 last_line = -1;
+        i64 jump_line_number = get_line_from_list(app, jump_state.list, i);
+        String_Const_u8 jump_line = push_buffer_line(app, scratch, jump_buffer_id, jump_line_number);
         
-        for(i32 i = 0; i < buffer_marker_count; i += 1)
+        // @note: Remove file part of jump line.
         {
-            i64 jump_line_number = get_line_from_list(app, jump_state.list, i);
-            i64 code_line_number = get_line_number_from_pos(app, buffer_id, buffer_markers[i].pos);
-            
-            if(code_line_number != last_line)
-            {
-                
-                String_Const_u8 jump_line = push_buffer_line(app, scratch, jump_buffer_id, jump_line_number);
-                
-                // NOTE(rjf): Remove file part of jump line.
-                {
-                    u64 index = string_find_first(jump_line, string_u8_litexpr("error"), StringMatch_CaseInsensitive);
-                    if(index == jump_line.size)
-                    {
-                        index = string_find_first(jump_line, string_u8_litexpr("warning"), StringMatch_CaseInsensitive);
-                        if(index == jump_line.size)
-                        {
-                            index = 0;
-                        }
-                    }
-                    jump_line.str += index;
-                    jump_line.size -= index;
-                }
-                
-                // NOTE(rjf): Render annotation.
-                {
-                    Rect_f32 region = text_layout_region(app, text_layout_id);
-                    Range_i64 line_range = Ii64(code_line_number);
-                    
-                    Range_f32 y1 = text_layout_line_on_screen(app, text_layout_id, line_range.min);
-                    Range_f32 y2 = text_layout_line_on_screen(app, text_layout_id, line_range.max);
-                    Range_f32 y = range_union(y1, y2);
-                    
-                    i64 line_end_pos = get_line_end_pos(app, buffer_id, code_line_number);
-                    Rect_f32 line_end_rect = text_layout_character_on_screen(app, text_layout_id, line_end_pos);
-                    f32 x0 = line_end_rect.x1;
-                    f32 string_x0 = (region.x1 - metrics.max_advance*jump_line.size - (y.max-y.min)/2 - metrics.line_height/2);
-                    if (string_x0 > x0)  x0 = string_x0;
-                    
-                    if(range_size(y) > 0.f) {
-                        draw_string(app, face_id, jump_line,
-                                    V2f32(x0,
-                                          y.min + (y.max-y.min)/2 - metrics.line_height/2),
-                                    0xffff0000);
-                    }
+            u64 index = string_find_first(jump_line, string_u8_litexpr("error"), StringMatch_CaseInsensitive);
+            if (index == jump_line.size) {
+                index = string_find_first(jump_line, string_u8_litexpr("warning"), StringMatch_CaseInsensitive);
+                if (index == jump_line.size) {
+                    index = 0;
                 }
             }
+            jump_line.str += index;
+            jump_line.size -= index;
+        }
+        
+        // @note: Render annotation.
+        {
+            Rect_f32 region = text_layout_region(app, text_layout_id);
+            Range_i64 line_range = Ii64(code_line_number);
             
-            last_line = code_line_number;
+            Range_f32 y1 = text_layout_line_on_screen(app, text_layout_id, line_range.min);
+            Range_f32 y2 = text_layout_line_on_screen(app, text_layout_id, line_range.max);
+            Range_f32 y = range_union(y1, y2);
+            
+            if (range_size(y) <= 0.0f) {
+                continue;
+            }
+            
+            Vec2_f32 draw_position = {
+                region.x1 - get_string_advance(app, face_id, jump_line),
+                y.min + (y.max-y.min)/2 - metrics.line_height/2
+            };
+            
+            i64 line_end_pos = get_line_end_pos(app, buffer_id, code_line_number);
+            Rect_f32 line_end_rect = text_layout_character_on_screen(app, text_layout_id, line_end_pos);
+            if (draw_position.x < line_end_rect.x1) {
+                draw_position.x = line_end_rect.x1;
+            }
+            
+            draw_string(app, face_id, jump_line, draw_position, 0xffff0000);
         }
     }
 }
+
 
 //
 // @note Draw comment divider horizontal lines
